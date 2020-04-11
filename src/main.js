b9870133bf605f2368c02da25878b8a1af7ce273
yoanad <yoana.dimova@gmail.com>
2020-04-11 23:29:09 +0100

    Make Paperdoll work with vue & refactor some things

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'normalize.css/normalize.css';
+import './assets/styles.scss'
 
 new Vue({
   el: '#app',
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: 'https://yoanad.github.io/dataVisBar',
     filename: 'build.js'
   },
   module: {
